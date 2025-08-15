7e17b6441ccd57c15e7c8583721818adbf0ad8df
Luiz Gustavo <dantas.lgf@gmail.com>
2025-08-14 21:46:08 -0300

    class OrderItem

--- src/main/java/com/dscommerce/devsuperior/entities/User.java
+++ src/main/java/com/dscommerce/devsuperior/entities/User.java
@@ -12,10 +12,16 @@ public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
+
     private String name;
+
+   @Column(unique = true )
     private String email;
+
     private String phone;
+
     private LocalDate birthDate;
+
     private String password;
 
     @OneToMany(mappedBy = "client")
