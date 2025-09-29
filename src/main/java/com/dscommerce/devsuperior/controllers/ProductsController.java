9dcada0242b4d8d40530aef40b3caa0311f2be63
Luiz Gustavo <dantas.lgf@gmail.com>
2025-09-29 20:44:26 -0300

    PUT products atualizar pro id

--- src/main/java/com/dscommerce/devsuperior/controllers/ProductsController.java
+++ src/main/java/com/dscommerce/devsuperior/controllers/ProductsController.java
@@ -7,11 +7,10 @@ import com.dscommerce.devsuperior.services.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Optional;
@@ -24,13 +23,30 @@ public class ProductsController {
     private ProductService service;
 
     @GetMapping(value = "/{id}")
-    public ProductDTO findBy(@PathVariable Long id){
-        return service.findById(id);
+    public ResponseEntity<ProductDTO> findBy(@PathVariable Long id){
+       ProductDTO dto = service.findById(id);
+        return ResponseEntity.ok(dto);
 
     }
     @GetMapping
-    public Page<ProductDTO> findAll(Pageable pageable){
-        return service.findAll(pageable);
+    public ResponseEntity<Page<ProductDTO>> findAll(Pageable pageable){
+       Page<ProductDTO> dto = service.findAll(pageable);
+        return ResponseEntity.ok(dto);
 
     }
+
+    @PostMapping
+    public ResponseEntity<ProductDTO> insert (@RequestBody ProductDTO dto){
+        ProductDTO criar = service.insert(dto);
+        return ResponseEntity.status(HttpStatus.CREATED).body(criar);
+
+    }
+
+    @PutMapping(value = "/{id}")
+    public ResponseEntity<ProductDTO> update(@PathVariable Long id, @RequestBody ProductDTO dto){
+         dto = service.update(id, dto);
+        return ResponseEntity.ok(dto);
+
+    }
+
 }
